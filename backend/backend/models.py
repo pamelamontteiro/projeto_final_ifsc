a9d9057a06d660c896b9dbdb10dad422a18bb532
Pamela Monteiro <pamela.csmonteiro@gmail.com>
2023-12-04 09:14:47 -0300

    Remove pyscript e remove unicidade dos nomes

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -8,7 +8,7 @@ class Usuario(Base):
     __tablename__ = "usuario"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, unique=True, index=True)
+    nome = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     senha = Column(String)
     tipo = Column(Integer)
