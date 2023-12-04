65358cbbe3d26b08ff290118e41d9deea9d18b2a
Pamela Monteiro <pamela.csmonteiro@gmail.com>
2023-12-04 09:07:23 -0300

    Adiciona CORS Middleware no backend

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import crud, models, schemas
@@ -7,7 +8,18 @@ from database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8001",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
